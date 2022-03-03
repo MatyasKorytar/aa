3374c5074bdb5e053c58eeaa5c412d7bbb92aed7
Matyáš Korytar <91247705+MatyasKorytar@users.noreply.github.com>
2022-03-03 11:47:24 +0100

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 RunComp.set_light_level()
 radio.set_group(24)
 def on_light_drop():
-    #cas = control.millis()
     radio.sendNumber(0)
 RunComp.on_light_drop(on_light_drop)
--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 RunComp.SetLightLevel()
 radio.setGroup(24)
 RunComp.OnLightDrop(function on_light_drop() {
-    // cas = control.millis()
     radio.sendNumber(0)
 })
